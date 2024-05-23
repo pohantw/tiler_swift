9fb9893b4f8ad5590d08dd6db2ac497a80e27444
Po-Han Chen <pohan@stanford.edu>
2024-05-22 22:43:36 -0700

    update benchmark generation script

--- benchmarks/gen_benchmarks.py
+++ benchmarks/gen_benchmarks.py
@@ -1,9 +1,10 @@
-import numpy as np
 import os
+import numpy
+import shutil
 from scipy.sparse import random
 
 # Seed for the random number generator
-np.random.seed(0)
+numpy.random.seed(0)
 
 # Dimensions of the matrices
 matrix_sizes = [10, 20, 40, 80, 160]
@@ -16,14 +17,17 @@ for matrix_size in matrix_sizes:
         rows = matrix_size
         cols = matrix_size
         # Generate a random sparse 2D matrix
-        matrixA = random(rows, cols, density=density, format='csr', random_state=np.random.default_rng())
-        matrixB = random(rows, cols, density=density, format='csr', random_state=np.random.default_rng())
+        matrixA = random(rows, cols, density=density, format='csr', random_state=numpy.random.default_rng())
+        matrixB = random(rows, cols, density=density, format='csr', random_state=numpy.random.default_rng())
         # Convert the sparse matrix to a dense matrix
         dense_matrixA = matrixA.toarray()
         dense_matrixB = matrixB.toarray()
         # create a folder
         folder = f'{matrix_size}x{matrix_size}_density{density}'
+        # remove the folder if it already exists
+        if os.path.exists(folder):
+            shutil.rmtree(folder)
         os.makedirs(folder, exist_ok=True)
         # Save the matrix to a .npy file
-        np.save(f'{folder}/matrixA.npy', dense_matrixA)
-        np.save(f'{folder}/matrixB.npy', dense_matrixB)
+        numpy.save(f'{folder}/A.npy', dense_matrixA)
+        numpy.save(f'{folder}/B.npy', dense_matrixB)
