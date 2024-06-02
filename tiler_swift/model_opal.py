d8e91dc7fab670fce9de6a5efed7e670867650b8
Po-Han Chen <pohan@stanford.edu>
2024-06-02 02:13:41 -0700

    1. fix coding style 2. fix tiling extraction 3. add tile overhead into runtime

--- tiler_swift/model_opal.py
+++ tiler_swift/model_opal.py
@@ -9,8 +9,10 @@ class Model_Opal:
         # because half of the capacity is used for seg
         # unit: number of elements
         self._out_mem_size = config['memory_capacity_mtile'] * 1024 / 2.0 / config['element_size']
-    
-    def _estimate_tile_runtime_elemadd( self, tiling_region ):
+
+
+    def _estimate_tile_runtime_elemadd( self, rect ):
+        x, y, width, height = rect
         # we use the number of output non-zero elements
         # to estimate the runtime, each non-zero output element
         # requires one unit of computation time
@@ -19,15 +21,16 @@ class Model_Opal:
             # for elementwise-add, the worst case is that
             # all input nnzs do not overlap, so we need to
             # add up all input nnzs
-            output_nnzs += numpy.count_nonzero( tensor[tiling_region] )
+            output_nnzs += numpy.count_nonzero( tensor[y:y+height, x:x+width] )
         if output_nnzs > self._out_mem_size:
             # we use negative value to indicate that the tiling is infeasible
             return -1
         else:
-            return output_nnzs
+            return self._config['tile_overhead'] + output_nnzs
         
         
-    def _estimate_tile_runtime_elemmul( self, tiling_region ):
+    def _estimate_tile_runtime_elemmul( self, rect ):
+        x, y, width, height = rect
         # we use the number of output non-zero elements
         # to estimate the runtime, each non-zero output element
         # requires one unit of computation time
@@ -37,18 +40,19 @@ class Model_Opal:
             # all input nnzs overlap, so the output size is
             # the maximum of all input sizes
             output_nnzs = max( output_nnzs,
-                               numpy.count_nonzero( tensor[tiling_region] ) )
+                               numpy.count_nonzero( tensor[y:y+height, x:x+width] ) )
         if output_nnzs > self._out_mem_size:
             # we use negative value to indicate that the tiling is infeasible
             return -1
         else:
-            return output_nnzs
+            return self._config['tile_overhead'] + output_nnzs
 
-    def estimate_tile_runtime( self, tiling_region ):
+
+    def estimate_tile_runtime( self, rect ):
         if self._config['operation'] == 'elementwise-add':
-            return self._estimate_tile_runtime_elemadd( tiling_region )
+            return self._estimate_tile_runtime_elemadd( rect )
         elif self._config['operation'] == 'elementwise-mul':
-            return self._estimate_tile_runtime_elemmul( tiling_region )
+            return self._estimate_tile_runtime_elemmul( rect )
         else:
             raise ValueError( 'Unsupported operation: ' + self._config['operation'] )
         
