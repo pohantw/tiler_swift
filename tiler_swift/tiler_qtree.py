2c9d9674a619e020763e59d0a7161bbaa31e85d1
Po-Han Chen <pohan@stanford.edu>
2024-06-02 02:39:00 -0700

    include model into qtree algorithm

--- tiler_swift/tiler_qtree.py
+++ tiler_swift/tiler_qtree.py
@@ -11,6 +11,30 @@ class Tiler_Qtree:
     def _tile_recursive( self, rect ):
         x, y, width, height = rect
 
+        # assumption: if the tile can have positive runtime estimate,
+        # then its runtime must be better than its splits
+        tile_runtime = self._model.estimate_tile_runtime( rect )
+        if tile_runtime < 0:
+            # the tile is infeasible, so we need to split it
+            if width > height:
+                hw = width // 2
+                r1, t1 = self._tile_recursive( [x, y, hw, height] )
+                r2, t2 = self._tile_recursive( [x + hw, y, width-hw, height] )
+                return r1 + r2, t1 + t2
+            else:
+                hh = height // 2
+                r1, t1 = self._tile_recursive( [x, y, width, hh] )
+                r2, t2 = self._tile_recursive( [x, y + hh, width, height-hh] )
+                return r1 + r2, t1 + t2
+        else:
+            result = {}
+            for tensor_name in self._tensors.keys():
+                result[tensor_name] = [x, y, width, height]
+            return tile_runtime, [result]
+
+    def _tile_recursive_simple( self, rect ):
+        x, y, width, height = rect
+
         # loop through tensors, count the non-zeros in the tile
         nnzs = {}
         for tensor_name, tensor in self._tensors.items():
@@ -47,10 +71,10 @@ class Tiler_Qtree:
                 hh = height // 2
             else:
                 hh = 1
-            q1, q1_is_leaf = self._tile_recursive( [x,      y,      hw,       hh] )
-            q2, q2_is_leaf = self._tile_recursive( [x + hw, y,      width-hw, hh] )
-            q3, q3_is_leaf = self._tile_recursive( [x,      y + hh, hw,       height-hh] )
-            q4, q4_is_leaf = self._tile_recursive( [x + hw, y + hh, width-hw, height-hh] )
+            q1, q1_is_leaf = self._tile_recursive_simple( [x,      y,      hw,       hh] )
+            q2, q2_is_leaf = self._tile_recursive_simple( [x + hw, y,      width-hw, hh] )
+            q3, q3_is_leaf = self._tile_recursive_simple( [x,      y + hh, hw,       height-hh] )
+            q4, q4_is_leaf = self._tile_recursive_simple( [x + hw, y + hh, width-hw, height-hh] )
 
             if self._config["qtree_tile_merging"]:
                 q_result = [[q1, q2], [q3, q4]]
@@ -161,5 +185,6 @@ class Tiler_Qtree:
         tensor_name = list(self._tensors.keys())[0]
         tensor_width = self._tensors[tensor_name].shape[1]
         tensor_height = self._tensors[tensor_name].shape[0]
-        result, _  = self._tile_recursive([0, 0, tensor_width, tensor_height])
+        # result, _  = self._tile_recursive_simple([0, 0, tensor_width, tensor_height])
+        run_time_estimate, result = self._tile_recursive([0, 0, tensor_width, tensor_height])
         return result
